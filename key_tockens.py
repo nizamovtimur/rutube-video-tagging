2cc4eef886a9a9b75872ef7d50b08ffc762c062e
Владислав Евгеньевич Сидоров <vlad.s200@mail.ru>
2024-09-28 04:52:50 +0500

    Fix bugs

--- key_tockens.py
+++ key_tockens.py
@@ -10,7 +10,7 @@ def get_key_tockens(text: str) -> str:
     Returns:
         str: строка ключевых слов
     """
-    rake_model = Rake(max_words=10, language_code="ru")
+    rake_model = Rake(max_words=10)
     key_tockens = ""
     for key_t in rake_model.apply(text)[:10]:
         key_tockens += key_t + "; "
